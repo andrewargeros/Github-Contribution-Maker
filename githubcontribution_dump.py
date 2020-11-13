7721d94185a6005c7857fbfedb5efc88cfcf8f2e
Andrew Argeros <aargeros15@gmail.com>
2020-11-13 10:06:46 -0600

    Initial Commit

---
+++ githubcontribution_dump.py
@@ -0,0 +1,31 @@
+import pyautogui
+import random
+import time
+
+def str_time_prop(start, end, format, prop):
+
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%Y-%m-%d', prop)
+
+time.sleep(5)
+
+pyautogui.typewrite("""cd C:/RScripts/contribution-dump
+""")
+time.sleep(1)
+pyautogui.typewrite('git init')
+
+for i in range(10):
+   date = random_date("2019-11-17", "2019-12-31", random.random())
+
+   message = f"""git commit --allow-empty --date "{date}" -m "{date}"
+   git push
+   """
+   pyautogui.typewrite(message)
